436957086f9a72900f64404beb040bdbd9e7e0a5
AryenN <70106602+AryenNN@users.noreply.github.com>
2020-09-04 17:16:42 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+//-----------------AryenN-----------------
+var title = "AryenN Roleplay";
+var link = "Discord:";
+//---------------AryenN---------------
+
+
+document.getElementById('title').innerHTML = title;
+document.getElementById('link').innerHTML = link;
+ 
+
+console.log('Connected.')
